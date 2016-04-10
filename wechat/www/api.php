c7406905c4c04a908802d8e98cb07a549b40d538
Timon <timonlio@gmail.com>
2016-04-10 17:38:53 +0800

    Support auto reply text message

--- wechat/www/api.php
+++ wechat/www/api.php
@@ -7,77 +7,88 @@
  */
 require_once("inc/config.php");
 
-$wechatObj = new wechatCallbackapiTest();
-$wechatObj->valid();
+if (isset($_GET['echostr'])) {
+    $echoStr = $_GET['echostr'];
 
-class wechatCallbackapiTest
+    if (checkSignature()) {
+	echo $echoStr;
+    } else {
+	echo "Failed";
+    }
+    exit;
+} else {
+    //get post data, May be due to the different environments
+    //$postStr = $_POST['HTTP_RAW_POST_DATA'];
+    $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+    //extract post data
+    if (!empty($postStr)) {
+	/*
+	 * libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+	 * the best way is to check the validity of xml by yourself
+	 */
+	libxml_disable_entity_loader(true);
+	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+	$msgId = $postObj->MsgId;
+	$msgType = $postObj->MsgType;
+	$fromUserName = $postObj->FromUserName;
+	$toUserName = $postObj->ToUserName;
+	$createTime = $postObj->CreateTime;
+	$content = trim($postObj->Content);
+
+	handleMessage($toUserName, $fromUserName, $createTime, $msgType, $content, $msgId);
+    }
+}
+
+function checkSignature()
 {
-    public function valid()
-    {
-        $echoStr = $_GET["echostr"];
+    global $config;
 
-        if ($this->checkSignature()) {
-	    echo $echoStr;
-	    exit;
-	}
+    if (!isset($config['wechat_token'])) {
+	throw new Exception('TOKEN not defined!');
     }
+    $token = $config['wechat_token'];
 
-    public function responseMsg()
-    {
-	//get post data, May be due to the different environments
-	$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+    $signature = isset($_GET['signature']) ? trim($_GET['signature']) : "";
+    $timestamp = isset($_GET['timestamp']) ? trim($_GET['timestamp']) : "";
+    $nonce = isset($_GET['nonce']) ? trim($_GET['nonce']) : "";
 
-      	//extract post data
-	if (!empty($postStr)) {
-	    /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-	       the best way is to check the validity of xml by yourself */
-	    libxml_disable_entity_loader(true);
-	    $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-	    $fromUsername = $postObj->FromUserName;
-	    $toUsername = $postObj->ToUserName;
-	    $keyword = trim($postObj->Content);
-	    $time = time();
-	    $textTpl = "<xml>
-		    <ToUserName><![CDATA[%s]]></ToUserName>
-		    <FromUserName><![CDATA[%s]]></FromUserName>
-		    <CreateTime>%s</CreateTime>
-		    <MsgType><![CDATA[%s]]></MsgType>
-		    <Content><![CDATA[%s]]></Content>
-		    <FuncFlag>0</FuncFlag>
-		    </xml>";             
-	    if (!empty($keyword)) {
-		$msgType = "text";
-		$contentStr = "Welcome to wechat world!";
-		$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-		echo $resultStr;
-	    } else {
-		echo "Input something...";
-	    }
-        } else {
-	    echo "";
-	    exit;
-        }
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING); // use SORT_STRING rule
+    $tmpStr = implode($tmpArr);
+    $tmpStr = sha1($tmpStr);
+
+    return ($tmpStr == $signature);
+}
+
+function handleMessage($toUserName, $fromUserName, $createTime, $msgType, $content, $msgId)
+{
+    $contentStr = strtolower(trim($content));
+    if ($contentStr == "func1") {
+	printResponse($fromUserName, $toUserName, "Response for func1");
+    } else if ($contentStr == "func2") {
+	printResponse($fromUserName, $toUserName, "Response for func2");
+    } else {
+	printResponse($fromUserName, $toUserName, "Hello world!");
     }
+}
 
-    private function checkSignature()
-    {
-	global $config;
-
-	if (!isset($config['wechat_token'])) {
-	    throw new Exception('TOKEN not defined!');
-	}
-	$token = $config['wechat_token'];
-
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-	$tmpArr = array($token, $timestamp, $nonce);
-	sort($tmpArr, SORT_STRING); // use SORT_STRING rule
-	$tmpStr = implode($tmpArr);
-	$tmpStr = sha1($tmpStr);
-
-	return ($tmpStr == $signature);
+function printResponse($toUserName, $fromUserName, $content)
+{
+    $createTime = time();
+    $msgType = "text";
+    $textTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[%s]]></MsgType>
+<Content><![CDATA[%s]]></Content>
+<FuncFlag>0</FuncFlag>
+</xml>";
+    if (!empty($content)) {
+	printf($textTpl, $toUserName, $fromUserName, $createTime, $msgType, $content);
+    } else {
+	echo "Input something...";
     }
 }
 
