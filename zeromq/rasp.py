9c45c115922e3dbc6e484f9213ca7011d6e8a629
Timon <timonlio@gmail.com>
2016-06-03 23:35:55 +0800

    Support load server address from config file

--- zeromq/rasp.py
+++ zeromq/rasp.py
@@ -16,29 +16,37 @@ logger.setLevel(logging.DEBUG)
 
 def main():
     logger.debug("main");
+
+    config_path = 'rasp.json'
+    config = load_config(config_path)
+    config_uuid = config.get('uuid', None);
+    config_server = config.get('server', 'localhost')
+    config_port_req = config.get('port_req', 5555)
+    config_port_sub = config.get('port_sub', 5556)
+
+    if config_uuid == None:
+        config_uuid = gen_uuid()
+        config['uuid'] = config_uuid
+        save_config(config_path, config)
+
+    logger.info("uid:%s" % config_uuid)
+
     context = zmq.Context()
 
     reqs = context.socket(zmq.REQ)
-    reqs.connect("tcp://localhost:5555")
+    reqs.connect("tcp://%s:%d" % (config_server, config_port_req))
 
     subs = context.socket(zmq.SUB)
-    subs.connect("tcp://localhost:5556")
+    subs.connect("tcp://%s:%d" % (config_server, config_port_sub))
     subs.setsockopt(zmq.SUBSCRIBE, "")
-
-    uid = gen_uuid()
-    logger.info("uid:%s" % uid)
 
     request = {}
     request['type'] = "REG"
-    request['uuid'] = uid
+    request['uuid'] = config_uuid
     reqs.send(json.dumps(request))
 
     response = reqs.recv()
     logger.debug("response:%s" % response);
-
-    #poller = zmq.Poller()
-    #poller.register(reqs, zmq.POLLIN)
-    #poller.register(subs, zmq.POLLIN)
 
     while True:
         handle(subs.recv())
@@ -52,17 +60,21 @@ def handle(message):
     wol.send_magic_packet(macaddr)
 
 def gen_uuid():
-    if os.path.isfile('rasp.conf'):
-        with open('rasp.conf', 'r') as f:
-            cache = json.load(f)
-            uid = cache['uuid']
+    return str(uuid.uuid1())
+
+def load_config(config_path):
+    config = {}
+    logging.info('load config %s' % config_path)
+    if os.path.isfile(config_path):
+        with open(config_path, 'r') as f:
+            return json.load(f)
     else:
-        uid = str(uuid.uuid1())
-        cache = {}
-        cache['uuid'] = uid
-        with open('rasp.conf', 'w') as f:
-            json.dump(cache, f)
-    return uid
+        return config
+
+def save_config(config_path, config):
+    logging.info('save config %s' % config_path)
+    with open(config_path, 'w') as f:
+        json.dump(config, f)
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
