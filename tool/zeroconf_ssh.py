675178e41719da258941449f8457a6500c43ad9a
Timon <timonlio@gmail.com>
2016-11-07 22:31:12 +0800

    Support use real ip and host name for service

--- tool/zeroconf_ssh.py
+++ tool/zeroconf_ssh.py
@@ -6,13 +6,15 @@ import time
 from zeroconf import *
 
 def main():
-    print "Register SSH service ..."
-
     service_type = "_ssh._tcp.local."
+    service_port = 22
+    service_addr = socket.gethostbyname(socket.gethostname())
+    service_name = socket.gethostname().replace('.local', '.')
     info = ServiceInfo(service_type,
-            "RPi3." + service_type,
-            socket.inet_aton("127.0.0.1"), 22,
+            service_name + service_type,
+            socket.inet_aton(service_addr), service_port,
             0, 0, "", None)
+    print "Register SSH service %s on %s ..." % (socket.gethostname(), service_addr)
 
     zc = Zeroconf()
     zc.register_service(info)
