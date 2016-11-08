ca2eaa6c653684faf8524173b794dda44f744818
Timon <timonlio@gmail.com>
2016-11-08 16:30:33 +0800

    On respi, gethostname not tain '.local' will failed to register

--- tool/zeroconf_ssh.py
+++ tool/zeroconf_ssh.py
@@ -9,12 +9,13 @@ def main():
     service_type = "_ssh._tcp.local."
     service_port = 22
     service_addr = socket.gethostbyname(socket.gethostname())
-    service_name = socket.gethostname().replace('.local', '.')
+    service_name = socket.gethostname().replace('.local', '')
+    service_name = service_name + '.' + service_type
+    print "Register SSH service [%s] on %s ..." % (service_name, service_addr)
     info = ServiceInfo(service_type,
-            service_name + service_type,
+            service_name,
             socket.inet_aton(service_addr), service_port,
             0, 0, "", None)
-    print "Register SSH service %s on %s ..." % (socket.gethostname(), service_addr)
 
     zc = Zeroconf()
     zc.register_service(info)
